cd49b783a5ce5893b862adb2807248f03a850d6f
Veliko <Kardziev>
2023-07-10 10:29:16 +0200

    code change after tipp in old PR

--- pages/entries/[id].js
+++ pages/entries/[id].js
@@ -1,19 +1,12 @@
 import Entry from "@/components/Entry";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import placeholderEntries from "@/lib/placeholderEntries";
 
 export default function EntryPage() {
   const router = useRouter();
+  const entry = placeholderEntries.find(
+    (placeholderEntry) => placeholderEntry.id === router.query.id
+  );
 
-  const [singleEntry, setSingleEntry] = useState("");
-  const [id, setId] = useState("");
-  useEffect(() => {
-    setId(router.query.id);
-    setSingleEntry(
-      placeholderEntries.find((placeholderEntry) => placeholderEntry.id === id)
-    );
-  });
-
-  return <>{singleEntry && <Entry data={singleEntry} />}</>;
+  return <>{entry && <Entry data={entry} />}</>;
 }
