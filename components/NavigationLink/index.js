cd6dc891cc076f84f3e2a69381f693a9d5533673
Veliko <Kardziev>
2023-07-12 15:53:50 +0200

    code change to navigation-components after review

--- components/NavigationLink/index.js
+++
@@ -1,5 +0,0 @@
-import { StyledNavigationLink } from "./NavigationLink.style";
-
-export default function NavigationLink({ href, children }) {
-  return <StyledNavigationLink href={href}>{children}</StyledNavigationLink>;
-}
