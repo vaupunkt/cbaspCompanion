f5497ca6df837567786c83abaf6e9940d2fc7ba2
Veliko <Kardziev>
2023-07-21 10:35:41 +0200

    trying to figure onClick event for Radar out

--- components/KieslerKreis/index.js
+++ components/KieslerKreis/index.js
@@ -1,5 +1,6 @@
 import { Radar } from "react-chartjs-2";
-import ChartJs from "chart.js";
+import Chart, { getAnglefFromPoint } from "chart.js/auto";
+import { getRelativePosition } from "chart.js/helpers";
 import styled from "styled-components";
 import { useState, useRef } from "react";
 
@@ -11,22 +12,22 @@ const DiagrammContainer = styled.div`
 `;
 
 export default function KieslerKreis() {
-  const chartRef = useRef();
   const [chartData, setChartData] = useState({
     labels: [
-      ["Dominant", "offen"],
+      ["Dominant", "Offen"],
       "",
       ["Freundlich", "Nähe"],
       "",
-      ["Unterwürfig", "verschlossen"],
+      ["Unterwürfig", "Verschlossen"],
       "",
       ["Feindselig", "Distanziert"],
       "",
     ],
     datasets: [
       {
+        type: "radar",
         label: "Daten",
-        data: [null, null, null, null, null, null, null, null],
+        data: [null, 2, null, null, null, null, null, null],
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
@@ -35,13 +36,6 @@ export default function KieslerKreis() {
   });
 
   const options = {
-    scale: {
-      ticks: {
-        beginAtZero: true,
-        max: 3,
-        stepSize: 1,
-      },
-    },
     scales: {
       r: {
         max: 3,
@@ -49,40 +43,21 @@ export default function KieslerKreis() {
         stepSize: 1,
       },
     },
-    onClick: (event) => {
-      if (chartRef.current) {
-        const chartInstance = chartRef.current.chartInstance;
-        const position = ChartJs.helpers.getRelativePosition(
-          event,
-          chartInstance
-        );
-        const scale = chartInstance.scales.r;
-        const angle = scale.getAngleFromPoint(position);
-        const index = Math.round((angle.angle - Math.PI / 2) / (Math.PI / 4));
-        const value = scale.getValueForPixel(
-          Math.sqrt(
-            Math.pow(position.x - scale.xCenter, 2) +
-              Math.pow(position.y - scale.yCenter, 2)
-          )
-        );
-        setChartData((prevData) => ({
-          ...prevData,
-          datasets: prevData.datasets.map((dataset) => ({
-            ...dataset,
-            data:
-              dataset.data[index] === null
-                ? dataset.data.map((v, i) => (i === index ? value : v))
-                : dataset.data.map((v) => null),
-          })),
-        }));
-      }
+    onClick: (event, element) => {
+      console.log(event);
+      console.log(element);
+      console.log(chartData.datasets[0].data);
+      const position = getRelativePosition(event);
+      const scale = options.scales.r;
+      const angle = scale.getAnglefFromPoint(position);
+      console.log("angle", angle);
     },
   };
 
   return (
     <>
       <DiagrammContainer>
-        <Radar ref={chartRef} data={chartData} options={options} />
+        <Radar data={chartData} options={options} />
       </DiagrammContainer>
     </>
   );
