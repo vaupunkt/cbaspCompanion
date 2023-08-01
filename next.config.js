54acff20e2a3962b6c7035223d46d8c1f5c7c1b5
Veliko <Kardziev>
2023-08-01 11:18:27 +0200

    change sizing of kieslerkreis

--- next.config.js
+++ next.config.js
@@ -4,23 +4,25 @@ const nextConfig = {
     styledComponents: true,
     reactStrictMode: true,
   },
+  experimental: {
+    forceSwcTransforms: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
+    withPWA({
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === "development",
+    });
 
     return config;
   },
 };
 
-module.exports = nextConfig;
 const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
+module.exports = nextConfig;
